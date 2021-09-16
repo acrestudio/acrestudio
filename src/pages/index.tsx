e8b3652934361013b444906ecd3fc62e360dfbdc
philsp <phil@9200.io>
2021-09-16 21:17:41 +0800

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Layout from '../components/Layout';
 import Meta from '../components/Meta';
+import Image from 'next/image';
 
 export default function IndexPage() {
   return (
     <Layout>
       <Meta />
       <h1>Hi!</h1>
+      <Image src="/images/sticker-mancat.png" alt="Picture of the author" />
     </Layout>
   );
 }
