d54d7733ab6cd2f29431d6933420fe2c7d82a1a4
philsp <phil@9200.io>
2021-09-29 23:54:20 +0800

    Update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const { nanoid } = require('nanoid');
 
 module.exports = {
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  },
   generateBuildId: async () => {
     const id = nanoid();
     process.env.BUILD_ID = id;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -33,6 +33,7 @@ const Layout: React.FC<{ title?: string; description?: string; image?: string }>
   return (
     <>
       <Head>
+        <html lang="en" />
         <meta charSet="utf-8" />
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
